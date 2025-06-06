c2c7b6ca76e5b5415391081f66e491575cd986a9
shrutinimai <nimaishruti@gmail.com>
2025-06-06 13:38:56 +0530

    integration

---
+++ userRoute.js
@@ -0,0 +1,10 @@
+
+const express = require('express');
+const authenticateUser = require("../controllers/authenticationControllers");
+
+const router = express.Router();
+
+router.post("/signup",authenticateUser.signUp);
+router.post("/login",authenticateUser.login);
+
+module.exports = router;
